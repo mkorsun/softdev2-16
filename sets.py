ef242801df571932102b4106d5d6660f0002e6ca
Max Korsun <mkorsun@stuy.edu>
2018-04-26 14:42:01 -0400

    finished

---
+++ sets.py
@@ -0,0 +1,25 @@
+def union(A, B):
+    join = [x for x in B if x not in A]
+    return A + join
+
+def intersect(A, B):
+    return[x for x in B if x in A]
+    
+def setdiff(A, B):
+    return[x for x in A if x not in B]
+
+def symdiff(A, B):
+    U = setdiff(A,B)
+    V = setdiff(B,A)
+    return union(U,V)
+
+def cartprod(A, B):
+    return[[x,y] for x in A for y in B]
+
+
+    
+print union([1,2,3],[2,3,4])
+print intersect([1,2,3],[2,3,4])
+print setdiff([1,2,3],[2,3,4])
+print symdiff([1,2,3],[2,3,4])
+print cartprod([1,2,3],[2,3,4])
